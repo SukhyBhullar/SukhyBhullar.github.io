31e324046c11fcb5fd8c2f3ff5a8cad609995e88
SukhyBhullar <beast698@hotmail.com>
2022-10-25 20:16:46 +0100

    Added Auth0

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
-import LoginButton from "../components/loginButton";
+import LoginButton from "../components/auth/loginButton";
 import { useAuth0 } from "@auth0/auth0-react";
 
 const Home: NextPage = () => {
