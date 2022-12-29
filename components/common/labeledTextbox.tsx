e2b4521f253e878cfa9fb92feec3476032855f8f
SukhyBhullar <beast698@hotmail.com>
2022-12-29 11:18:31 +0000

    PLAN-25: Fixed special chars in textbox

--- components/common/labeledTextbox.tsx
+++ components/common/labeledTextbox.tsx
@@ -1,21 +1,26 @@
-import React, { ChangeEventHandler } from 'react'
+import React, { ChangeEventHandler } from "react";
 
 type Props = {
-    label: string,
-    onChange: ChangeEventHandler<HTMLInputElement>,
-    maxLength?: number
-}
+  label: string;
+  onChange: ChangeEventHandler<HTMLInputElement>;
+  maxLength?: number;
+};
 
-export default function LabeledTextbox({label, onChange, maxLength}: Props) {
+export default function LabeledTextbox({ label, onChange, maxLength }: Props) {
+  const restrictChars: React.FormEventHandler<HTMLInputElement> = (e) => {
+    e.currentTarget.value = e.currentTarget.value.replace(/[^a-zA-Z0-9]/g, "");
+  };
+
   return (
     <div className="flex flex-row gap-3 items-center">
-    <label className="label-text">{label}</label>
-    <input 
-      onChange={onChange}
-      type="text"
-      className="input input-bordered w-full max-w-xs"
-      maxLength={maxLength}
-    ></input>
-  </div>
-  )
+      <label className="label-text">{label}</label>
+      <input
+        onChange={onChange}
+        type="text"
+        className="input input-bordered w-full max-w-xs"
+        maxLength={maxLength}
+        onInput={restrictChars}
+      ></input>
+    </div>
+  );
 }
